977611bed42eb00c3b78d56a9cfa3ca6ea151b62
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-19 03:43:05 +0300

    Add files via upload

---
+++ JavaScript/patika-dev_javascript_egitimi/hello-world/hello-world.js
@@ -0,0 +1,10 @@
+//  MERHABA DÜNYA ÖRNEKLERİ
+
+// Sayfaya / Ekrana / Dökümana yazdırmak için:
+// document.write("Merhaba Dünya");
+
+// Uyarı Mesajı (Alert) ile yazdırmak için:
+// alert("Merhaba Dünya");
+
+// Konsol ile yazdırmak için:
+// console.log("Merhaba Dünya");
