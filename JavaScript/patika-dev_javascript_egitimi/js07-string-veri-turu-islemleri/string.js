a9b00e2426dc3911abdd14772ab751d300bc40fc
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-24 07:35:30 +0300

    Add files via upload

---
+++ JavaScript/patika-dev_javascript_egitimi/js07-string-veri-turu-islemleri/string.js
@@ -0,0 +1,81 @@
+// String veri türü işlemleri:
+
+let email="berkanyildiz@gmail.com"
+let firstName="berkan"
+let lastName="YILDIZ"
+
+// string karakter sayısı => length
+console.log(email.length)
+
+// İlk karakteri bulmak => [0]:
+console.log(firstName[0]) // Output => b
+console.log(firstName.charAt(0)) // Output => b
+
+// Büyük / Küçük harf:
+firstName=firstName.toUpperCase()
+console.log(firstName)
+
+firstName=firstName.toLowerCase()
+console.log(firstName)
+
+// String içinde istediğimiz bilgiyi aramak ver yerini bulmak => search:
+console.log(email.search("@")) // Output => @
+console.log(email[12]) // Output => @
+
+console.log(email.search("olmayan")) // Output => -1
+
+// Belli bir yere kadarını al => slice:
+console.log(email.slice(10)) // Output => iz@gmail.com
+console.log(email.slice(10,11)) // Output => i
+let DOMAIN=email.slice(email.search("@")+1)
+console.log(DOMAIN) // Output => gmail.com
+
+console.log(
+    DOMAIN.slice(0, DOMAIN.indexOf('.')) // Sadece gmail kısmını aldık
+)
+
+// Bilgiyi değiştir => replace:
+email=email.replace("gmail.com","kodluyoruz.org")
+console.log(email)
+
+// İstediğim bilgi var mı? => includes:
+email.includes("awopkdapodks") // false
+email.includes("@") // true
+
+// İstediğim bilgiyle başladı mı? => startsWith / bitti mi? => endsWith
+console.log(
+    email.endsWith("kodluyoruz.org") // True
+)
+console.log(
+    email.endsWith("kodluyoruz.com") // False
+)
+console.log(
+    email.startsWith("berkan") // True
+)
+console.log(
+    email.startsWith("yildiz") // False
+)
+
+// İlk harflerini büyük yapmak:
+firstName="FIRST"
+lastName="LAST"
+let fullName=`${firstName[0].toUpperCase()}${firstName.slice(1).toLowerCase()}
+${lastName[0].toUpperCase()}${lastName.slice(1).toLowerCase()}
+`
+console.log(fullName)
+// Output:
+// "First"
+// "Last"
+
+
+
+
+
+
+
+
+
+
+
+
+
