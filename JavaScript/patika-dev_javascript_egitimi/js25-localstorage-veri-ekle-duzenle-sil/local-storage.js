49fee5963bae605dd7d468c6edb2faae6823970a
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-25 06:22:31 +0300

    Add files via upload

---
+++ JavaScript/patika-dev_javascript_egitimi/js25-localstorage-veri-ekle-duzenle-sil/local-storage.js
@@ -0,0 +1,7 @@
+// Local Storage => Yerel Deoplama
+
+localStorage.setItem("key","value")
+localStorage.setItem("myCat","Kedddyyy") // Ayarlama / Oluşturma işlemi
+
+localStorage.getItem("myCat") // Çağırma işlemi
+localStorage.removeItem("myCat") // Silme İşlemi
