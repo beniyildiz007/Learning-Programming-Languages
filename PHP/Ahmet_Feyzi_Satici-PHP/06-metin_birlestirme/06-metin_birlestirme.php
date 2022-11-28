9ce8ec5dda29560c7e87d0a01c10819da4039890
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-28 16:28:57 +0300

    Add files via upload

---
+++ PHP/Ahmet_Feyzi_Satici-PHP/06-metin_birlestirme/06-metin_birlestirme.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo("PHP Ders-6") ?></title>
+</head>
+<body>
+<div>
+    <p><?php
+    $num1 = 12;
+    $num2 = 19;
+    echo("$num1 + $num2 = ".($num1+$num2)."<br>");
+    echo("\r\n$num1 - $num2 = ".($num1-$num2)."<br>");
+    echo("\r\n$num1 * $num2 = ".($num1*$num2)."<br>");
+    echo("\r\n$num1 / $num2 = ".($num1/$num2));
+    ?></p>
+</div>
+</body>
+</html>
+
+
