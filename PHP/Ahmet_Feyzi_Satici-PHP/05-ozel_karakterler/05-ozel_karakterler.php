66eaad534be2f0277a3e7d08f0081e2c8deb7925
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-28 16:26:16 +0300

    Add files via upload

---
+++ PHP/Ahmet_Feyzi_Satici-PHP/05-ozel_karakterler/05-ozel_karakterler.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo("PHP Ders-5") ?></title>
+</head>
+<body>
+<div>
+    <p><?php echo("Berkan Nihat Yıldız"); echo("<br>"); echo(" \r\nMarmara Üniversitesi") ?></p>
+    <!-- \r\n ifadesi görünürde etkisi olmaz fakat kaynak dosyada metni bir alt satıra geçirir! -->
+</div>
+</body>
+</html>
+
+
