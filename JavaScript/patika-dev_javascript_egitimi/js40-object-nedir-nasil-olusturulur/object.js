892b0bd29392af7d52d09322ae6de746137da817
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-11-29 10:44:14 +0300

    Add files via upload

---
+++ JavaScript/patika-dev_javascript_egitimi/js40-object-nedir-nasil-olusturulur/object.js
@@ -0,0 +1,18 @@
+// Object (nesne) nedir? Nasıl oluşturulur?
+
+let arrayObj = [1,2,3]
+let object1={obj: 1}
+
+console.log("arrayObj", typeof(arrayObj))
+console.log("object1", typeof(object1))
+
+let item1 = new Object()
+let item2 = {}
+console.log("item2", typeof(item2))
+console.log("item1", typeof(item1)) // iki sonuç da aynı
+
+// Object oluşturmak:
+let item3 = {}
+let item4 = new Object()
+
+console.log("object1", object1)
