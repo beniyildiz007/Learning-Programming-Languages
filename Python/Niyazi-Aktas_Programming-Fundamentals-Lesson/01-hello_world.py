48a94f8524672f966f6c9844cf5fff715ff32dec
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2022-12-01 07:29:47 +0300

    Add files via upload

---
+++ Python/Niyazi-Aktas_Programming-Fundamentals-Lesson/01-hello_world.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+# Output => Hello World
+
+
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+# Output:
+"""Hello World!
+Hello World!
+Hello World!"""
+
+
+
+
+w = "python"
+n = 10
+
+print(w.capitalize()) # capitalize metodu cümlenin ilk harini büyük yapar
+# Output => Python
+
+print(n.bit_length()) # bit_length metodu İkili sistemde bir tamsayıyı temsil etmek için gereken bit sayısını verir
+# Output => 4
+
+help(w.capitalize) # Help metodu ise mevcut fonksiyon ile ilgili yardımcı bilgiler verir
+# Output:
+"""Help on built-in function capitalize:
+
+capitalize() method of builtins.str instance
+    Return a capitalized version of the string.
+
+    More specifically, make the first character have upper case and the rest 
+lower
+    case."""
+
