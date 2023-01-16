31fe60b3e91444234a1f7b6128eaa3117f17f71c
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-01-16 21:13:05 +0300

    Add files via upload

---
+++ C#/C#-Console/01-Merhaba_Dunya/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merhaba_Dunya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba Dünya"); /*Ekrana yazı yazdırdık*/
+            Console.Read(); /*Ekranın açık kalmasını sağladık*/
+
+            Console.WriteLine("C# Console Dersleri"); /*Cursor'ün aşağıda bitmesini sağladık*/
+            Console.Read();
+        }
+    }
+}
