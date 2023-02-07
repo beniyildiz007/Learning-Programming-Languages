a7bf546cadb9033422913255adef107947f1b38d
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-07 16:11:57 +0300

    Add files via upload

---
+++ C#/C#-Console/11-Donguler_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Donguler_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //120 Sayısının Bölenleri:
+            int sayi = 120;
+            for(int i = 1; i < 120; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+            */
+
+
+            int a = 1, b = 1;
+            int c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for(int i = 1; i <= 8; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
