01bc5a6062d18cc2ca8e55673a1308284bad86c7
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-07 16:12:15 +0300

    Add files via upload

---
+++ C#/C#-Console/12-While_Dongusu/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_While_Dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int i;
+            i = 1;
+            while (i < 10)
+            {
+                Console.WriteLine("Hey Sen!");
+                i++;
+            }
+            Console.ReadLine();
+            */
+
+
+            /*
+            int i;
+            i = 1;
+            while (i < 400)
+            {
+                Console.WriteLine(i);
+                i++;
+                //1-399 arası bütün sayılar yazılır, 400 YAZILMAZ!
+            }
+            Console.ReadLine();
+            */
+
+
+            /*
+            int sayi;
+            Console.WriteLine("Bir sayı girin: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            while (sayi % 2 == 0)
+            {
+                Console.WriteLine("Bir sayı girin: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Tek sayı girdiniz!");
+            Console.ReadLine();
+            */
+
+
+            
+            int sayi;
+            Console.WriteLine("Faktoriyeli alınacak sayıyı girin:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            int fakt = 1;
+
+            while (sayi > 1)
+            {
+                fakt = fakt * sayi;
+                sayi--;
+            }
+            Console.WriteLine(fakt);
+            Console.Read();
+
+        }
+    }
+}
