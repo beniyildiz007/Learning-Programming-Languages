9d35de695a98ba706504142a16fb42d7bdb19d7d
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-08 23:58:56 +0300

    Add files via upload

---
+++ C#/C#-Console/15-Diziler_3_Foreach_Dongusu/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Diziler_3_Foreach_Dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] sehirler = { "Mersin", "İzmir", "Kocaeli", "Artvin", "Ankara", "Erzurum", "Şanlıurfa" };
+
+            foreach(string i in sehirler)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            */
+
+
+            /*
+            int[] sayilar = { 1, 4, 8, 9, 10 };
+            int toplam = 0;
+            foreach(int i in sayilar)
+            {
+                toplam += i;
+            }
+            Console.Write("Toplam= " + toplam);
+            Console.ReadLine();
+            //Output = 32
+            */
+
+
+            /*
+            int[] rakamlar = { 1, 7, 5, 4, 8, 6, 9, 3 };
+            foreach(int sayi in rakamlar)
+            {
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine(sayi);
+                }
+            }
+            Console.ReadLine();
+            */
+
+
+
+            int[] degerler = { 1, 2, 3, 4, 5, 6 };
+            int sonuc = 1;
+            foreach(int i in degerler)
+            {
+                sonuc *= i;
+            }
+            Console.WriteLine("6 Sayısının faktöriyeli: " + sonuc);
+            Console.ReadLine();
+        }
+    }
+}
