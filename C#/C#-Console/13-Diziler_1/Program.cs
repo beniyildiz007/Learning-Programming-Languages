39f36b9b922587906d1cfbc867278c80ef2320a6
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-08 23:57:59 +0300

    Add files via upload

---
+++ C#/C#-Console/13-Diziler_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Diziler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] sayilar = { 10, 45, 21, 33, 54, 66, 77, 99 };
+            Console.WriteLine(sayilar[3]);
+            Console.Read();
+            //Output = 33
+            */
+
+
+            /*
+            string[] isimler = { "ali", "ahmet", "emel", "ayşe", "mehmet", "baran" };
+            Console.WriteLine(isimler[0]);
+            Console.Read();
+            //Output = ali
+            */
+
+
+            
+            //Yazılan şehir isimlerini diziye aktarma
+            string[] sehirler = new string[3];
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write("Şehir: ");
+                sehirler[i] = Console.ReadLine();
+            }
+            Console.WriteLine("*** Şehirler Listesi ***");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(sehirler[i]);
+            }
+            Console.Read();
+            
+        }
+    }
+}
