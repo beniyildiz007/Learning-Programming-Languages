03d87ea7d65af29c43ab43804da5fd14c1790edc
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-11 14:09:59 +0300

    Add files via upload

---
+++ C#/C#-Console/22-Bool_Degiskeni/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_Bool_Degiskeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int sayi;
+            Console.Write("Sayıyı girin:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            bool durum1 = sayi > 0;
+            bool durum2 = sayi > 10;
+            Console.WriteLine("Sayı Pozitif mi {0}", durum1);
+            Console.WriteLine("Sayı 10'dan büyük mü {0}", durum2);
+            Console.ReadLine();
+            */
+
+
+
+            Console.Write("Şifreyi Girin: ");
+            int sifre;
+            sifre = Convert.ToInt32(Console.ReadLine());
+            bool durum1 = sifre == 1234;
+            Console.WriteLine("Şifre Doğrumu {0}", durum1);
+            Console.ReadLine();
+
+        }
+    }
+}
