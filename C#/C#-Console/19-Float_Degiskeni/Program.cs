f8bc293df4b866e9271197dc5b2590342033cbb1
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-11 14:09:01 +0300

    Add files via upload

---
+++ C#/C#-Console/19-Float_Degiskeni/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Float_Degiskeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            float sayi;
+            sayi = 123;
+            Console.WriteLine("Sayı: " + sayi);
+            Console.ReadLine();
+            */
+
+
+            /*
+            float s1, s2, toplam;
+            s1 = 25;
+            s2 = 45;
+            toplam = s1 + s2;
+            Console.WriteLine("Toplam: " + toplam);
+            Console.ReadLine();
+            */
+
+
+            /*
+            float deger;
+            deger = 1234567; //Float hassasiyet değeri 7 basamaklı sayılar kadardır
+            Console.WriteLine(deger);
+            Console.Read();
+            //Output = 1234567
+            */
+
+
+            /*
+            float sayi;
+            sayi = 2.123456f;
+            Console.WriteLine(sayi);
+            Console.Read();
+            */
+
+
+
+            int x = 3;
+            float y = 2.5f;
+            int z = 4;
+            float toplam;
+            toplam = x + y + z;
+            Console.WriteLine("Toplam: " + toplam);
+            Console.Read();
+            //Output = 9,5
+
+        }
+    }
+}
