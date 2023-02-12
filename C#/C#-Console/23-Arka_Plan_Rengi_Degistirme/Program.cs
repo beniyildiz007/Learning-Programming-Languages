7ce4175cd6a35480464e2fa1a03fd79525dfe328
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-12 12:46:22 +0300

    Add files via upload

---
+++ C#/C#-Console/23-Arka_Plan_Rengi_Degistirme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Arka_Plan_Rengi_Degistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.BackgroundColor = ConsoleColor.Blue; //Arkaplan rengimizi Mavi yaptık
+            Console.ForegroundColor = ConsoleColor.Black; //Yazımızın rengini siyah yaptık
+            Console.Title = "Çanakkale Zaferi"; //Console penceremizin başlığını değiştirdik
+            Console.Clear();
+            Console.WriteLine("18 Mart Çanakkale Şehitlerini Anma Günü");
+
+            Console.ReadLine();
+            */
+
+
+
+
+            Console.Write("Lütfen bir sayı girin: ");
+            ConsoleColor renk = (ConsoleColor)Convert.ToInt32(Console.ReadLine());
+            Console.BackgroundColor = renk;            
+            Console.Clear();
+            Console.ReadLine();
+            //0-15 arasında bir sayı girilmeli çünkü 16 tane renk paleti tanımlanmış durumda
+
+
+        }
+    }
+}
---
+++ C#/C#-Console/23-Arka_Plan_Rengi_Degistirme/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("23-Arka_Plan_Rengi_Degistirme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("23-Arka_Plan_Rengi_Degistirme")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ccc3d83-5834-4843-b344-9c1e6d34abb7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
