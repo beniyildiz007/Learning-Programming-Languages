00930b5b5637607dac8dbf05874a7632a18283ae
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-13 01:20:12 +0300

    Add files via upload

---
+++ C#/C#-Console/27-Matematiksel_Fonksiyonlar/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Matematiksel_Fonksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int sayi;
+            Console.Write("Sayıyı girin: ");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Mutlak Değerli Hali: " + Math.Abs(sayi));
+            Console.ReadLine();
+            */
+
+
+            /*
+            double sayi;
+            Console.Write("Sayıyı Girin: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sayının Üste Yuvarlanmış Hali: " + Math.Ceiling(sayi));
+            Console.ReadLine();
+            */
+
+
+
+            /*
+            double sayi;
+            Console.Write("Sayıyı Girin: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sayının Alta Yuvarlanmış Hali: " + Math.Floor(sayi));
+            Console.ReadLine();
+            */
+
+
+
+            /*
+            //Girilen sayının 5. kuvvetini almak
+            double sayi;
+            Console.Write("Sayıyı Girin: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sonuç: " + Math.Pow(sayi, 5));
+            Console.ReadLine();
+            */
+
+
+
+
+            //Girilen sayının karekökünü bulmak
+            double sayi;
+            Console.Write("Sayıyı Girin: ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sonuç: " + Math.Sqrt(sayi));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
