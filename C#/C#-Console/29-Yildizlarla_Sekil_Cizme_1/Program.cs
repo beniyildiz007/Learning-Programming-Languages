c43a7a4389d0557ba8de4ee0a6d0044e1e67166d
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-15 07:36:23 +0300

    Add files via upload

---
+++ C#/C#-Console/29-Yildizlarla_Sekil_Cizme_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Yildizlarla_Sekil_Cizme_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //10x10 dik üçgen çizdirmek v1:
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(new String('*',i)); //new String parametresi içerisinde önce yazdırmak istediğimiz değeri sonra da adedini giriyoruz
+            }
+            Console.ReadLine();
+            */
+
+
+
+            /*
+            //10x10 dik üçgen çizdirmek v2:
+            for (int i = 1; i <= 10; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            */
+
+
+
+
+            //10x10 dik üçgen tersten çizdirmek:
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 10; j >= i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
