3d501a732fa11ae14de5355aa3850efec6f1b4d8
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-15 07:39:15 +0300

    Add files via upload

---
+++ C/Kaynak-01/01-Merhaba_Dunya/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	/*
+	printf("Merhaba Dunya"); // C dilinde türkçe karakterler desteklenmez!
+	
+	printf(" 13 Subat 2023 Istanbul"); // Her yeni yazýlan komuttan sonra deðiþikliklerin uygulanmasý için Compile edlimesi gerekir!
+	
+	printf("\nC Programlama Dilini ogrenmeye basladik."); // \n komutu bir alt satýra geçmeyi saðlar.
+	*/
+	
+	
+	printf("******** Kisi Bilgileri ********");
+	printf("\n\n");
+	printf("Ad: Berkan Nihat\n");
+	printf("Soyad: Yildiz\n");
+	printf("Meslek: Ogretmen\n");
+	printf("Maas: 15.000 TL\n");
+	printf("Semt: Yenimahalle\n\n");
+	printf("*********************************");
+	
+	return 0;
+}
