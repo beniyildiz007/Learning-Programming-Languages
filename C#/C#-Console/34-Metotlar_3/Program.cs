efa546229b8a54b52b6a3f4b9377a36b48227efe
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-16 01:47:08 +0300

    Add files via upload

---
+++ C#/C#-Console/34-Metotlar_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_Metotlar_3
+{
+    class Metotlar //Başlangıçta Program olan gelen class adını değiştirdik
+    {
+
+        int topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Metotlar mtr = new Metotlar(); //Önce mtr adında Metotlar sınıfından bir nesne türettik
+            int toplam = mtr.topla(1, 3); //Daha sonra ise bu nesneyi kullanarak topla() metodunu kullandık
+            Console.WriteLine("Toplam: " + toplam);
+            Console.ReadLine();
+            //Output = Toplam: 4
+            
+        }
+    }
+}
