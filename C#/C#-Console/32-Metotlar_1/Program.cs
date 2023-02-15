a5bf5cd1b2928e39627571f2dc64567c69967790
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-16 01:46:19 +0300

    Add files via upload

---
+++ C#/C#-Console/32-Metotlar_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_Metotlar_1
+{
+    class Program
+    {
+
+        private static void veriler()
+        {
+            Console.WriteLine("Müdür: Berkan Nihat Yıldız");
+            Console.WriteLine("Öğretmen: Ecmel Yıldız");
+            Console.WriteLine("Okul: Atatürk Ortaokulu");
+            Console.WriteLine("Eğitsel Kol: Kütüphanecilik");
+            Console.WriteLine("Şehir: Antalya");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+        }
+        static void Main(string[] args)
+        {
+            veriler();
+            Console.Read();
+        }
+    }
+}
