0c970ffe21396e74b998ad9c7796d64c89ca4095
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-15 07:37:06 +0300

    Add files via upload

---
+++ C#/C#-Console/31-Yildizlarla_Sekil_Cizme_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Yildizlarla_Sekil_Cizme_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Büyüklüğü girilen baklava dilimin çizdirmek
+            Console.Write("Baklava Dilimi Büyüklüğü: ");
+            int dilim = Convert.ToInt32(Console.ReadLine());
+
+            //Üst kısım
+            for (int i = 1; i <= dilim; i++)
+            {
+                for (int j = dilim; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            //Alt kısım
+            for (int i = 1; i <= dilim; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = dilim; k > i; k--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
