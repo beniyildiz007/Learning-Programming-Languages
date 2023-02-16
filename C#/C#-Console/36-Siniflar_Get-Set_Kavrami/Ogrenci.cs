b6aca1547762bfd47403f7ae07182c1500f8e938
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-17 01:59:15 +0300

    Add files via upload

---
+++ C#/C#-Console/36-Siniflar_Get-Set_Kavrami/Ogrenci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_Siniflar_Get_Set_Kavrami
+{
+    class Ogrenci
+    {
+
+        private string ad;
+        private string soyad;
+        private int yas;
+        private string alan;
+
+        public string ADI
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string SOYADI
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+        public string ALANI
+        {
+            get { return alan; }
+            set { alan = value; }
+        }
+        public int YASI
+        {
+            get { return yas; }
+            set
+            {
+                if (value < 18)
+                {
+                    yas = 18;
+                }
+                else
+                {
+                    yas = value;
+                }
+            }
+        }
+    }
+}
---
+++ C#/C#-Console/36-Siniflar_Get-Set_Kavrami/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_Siniflar_Get_Set_Kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ogrenci ogr = new Ogrenci();
+            ogr.ADI = "Berkan Nihat";
+            ogr.SOYADI = "Yıldız";
+            ogr.ALANI = "Mateamtik-Fen";
+            ogr.YASI = 15; //Yaş otomatik olarak 18 gelecektir
+
+            Console.WriteLine("Adı: " + ogr.ADI);
+            Console.WriteLine("Soyadı: " + ogr.SOYADI);
+            Console.WriteLine("Alanı: " + ogr.ALANI);
+            Console.WriteLine("Yaşı: " + ogr.YASI);
+
+            Console.ReadLine();
+        }
+    }
+}
