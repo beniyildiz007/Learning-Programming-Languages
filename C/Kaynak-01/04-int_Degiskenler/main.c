fd92e6d71c54b5e7b1d71aac4f408c220cf5fe67
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-18 00:30:13 +0300

    Add files via upload

---
+++ C/Kaynak-01/04-int_Degiskenler/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	/*
+	int sayi;
+	printf("%d",sayi); // Rakamsal ifadeleri belirtmek için %d kullanýlýr (d = decimal)
+	//Output = 0
+	//Eðer sayýsal bir deðiþkene hiçbir deðer atamazsak varsayýlan deðeri 0 olarak yazar.
+	*/
+	
+	
+	
+	/*
+	// Belirlenmiþ olan iki sayýyý toplatma
+	int s1,s2,toplam;
+	s1=40;
+	s2=80;
+	toplam=s1+s2;
+	
+	printf("Toplam Sonuc: %d",toplam);
+	*/
+	
+	
+	
+	
+	
+	/*
+	//Dört iþlem yaptýrmak
+	int s1,s2,topla,carp,fark,bol;
+	
+	s1=50;
+	s2=10;
+	
+	topla=s1+s2;
+	fark=s1-s2;
+	carp=s1*s2;
+	bol=s1/s2;
+	
+	printf("Toplam: %d\n",topla);
+	printf("Fark: %d\n",fark);
+	printf("Carpim: %d\n",carp);
+	printf("Bolum: %d",bol);
+	*/
+	
+	
+	
+	
+	
+	/*
+	//Karenin alan ve çevresi
+	int kenar,alan,cevre;
+	kenar=10;
+	alan=kenar*kenar;
+	cevre=kenar*4;
+	printf("Alan: %d\n",alan);
+	printf("Cevre: %d",cevre);
+	*/
+	
+	
+	
+	
+	
+	
+	//Dikdörtgen alan ve çevresi
+	int kisa,uzun,alan,cevre;
+	kisa=8;
+	uzun=12;
+	alan=kisa*uzun;
+	cevre=2*(kisa+uzun);
+	printf("Alan: %d\n",alan);
+	printf("Cevre: %d",cevre);	
+	
+	
+	
+	
+	return 0;
+}
