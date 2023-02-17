daacebe4d9477c957bf4a2fc959d922d13cb2b24
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-18 00:28:41 +0300

    Add files via upload

---
+++ SQL - MySQL/KAYNAK-1/07-Delete.sql
@@ -0,0 +1,8 @@
+
+--Dersler tablosunun içerisindeki bütün verileri sil:
+--delete from TBL_DERSLER
+
+
+
+--Dersler tablosundaki Algoritma dersini sil:
+delete from TBL_DERSLER where DERS_AD='Algoritma'
