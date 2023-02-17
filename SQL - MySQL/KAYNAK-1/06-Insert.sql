23d9efd24d009e4d8f777f8857f4fefee97345f4
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-18 00:28:30 +0300

    Add files via upload

---
+++ SQL - MySQL/KAYNAK-1/06-Insert.sql
@@ -0,0 +1,8 @@
+
+--Öðrenciler tablosuna ismi Berkan Nihat, soyadý Yýldýz ve þehri Antalya olan veriyi ekledi
+--insert into [TBL-OGRENCILER](OGR_AD,OGR_SOYAD,OGR_SEHIR) values ('Berkan Nihat','Yýldýz','Antalya')
+
+
+--Notlar tablosuna sayýsal deðerlerin girilmesi:
+insert into TBL_NOTLAR (OGRENCI,DERS,SINAV1,SINAV3) values (6,2,88,67)
+
