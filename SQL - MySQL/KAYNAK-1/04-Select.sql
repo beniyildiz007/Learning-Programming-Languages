743747e3e9eebb26756bc959dd48cbb89e6618cf
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-18 00:27:57 +0300

    Add files via upload

---
+++ SQL - MySQL/KAYNAK-1/04-Select.sql
@@ -0,0 +1,9 @@
+--Öðrenciler tablosundaki bütün verileri getir:
+--Select * From [TBL-OGRENCILER]
+
+--Öðrenciler tablosundaki sadece Ad ve Soyad sütunlarýný getir:
+--Select OGR_AD, OGR_SOYAD From [TBL-OGRENCILER]
+
+
+--Öðrenciler tablosundaki adý Ali olanlarý getir
+Select * From [TBL-OGRENCILER] where OGR_AD='Ali'
