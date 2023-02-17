7480b49185f1585e54d501a3453c37ad77264bc4
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-18 00:28:51 +0300

    Add files via upload

---
+++ SQL - MySQL/KAYNAK-1/08-Truncate.sql
@@ -0,0 +1,9 @@
+
+
+--Dersler tablosundaki bütün verileri silmek için:
+--truncate table TBL_DERSLER
+
+--Delete ve Truncate arasýndaki temel farklýlýk:
+--Delete verileri silerken ID deðerlerini sýfýrlamaz ve kaldýðý yerden devam eder
+--Truncate verileri silerken bütün ID deðerlerini de sýfýrlar
+--Truncate komutu, iliþkisel tablolarda sorunlara yol açabilir
