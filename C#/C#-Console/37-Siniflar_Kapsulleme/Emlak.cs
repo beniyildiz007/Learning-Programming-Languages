eacfbfd0f8cd4b3fc8dd265d1053da0ed49a4aa1
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-19 18:57:31 +0300

    Add files via upload

---
+++ C#/C#-Console/37-Siniflar_Kapsulleme/Emlak.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Siniflar_Kapsulleme
+{
+    class Emlak
+    {
+
+        private string semt;
+        private string renk;
+        private int odasayisi;
+        private int katnumara;
+        private double alan;
+
+        public string SEMT
+        {
+            get { return semt; }
+            set { semt = value.ToUpper(); }
+        }
+        
+        public string RENK
+        {
+            get { return renk; }
+            set { renk = value.ToLower(); }
+        }
+
+        public int ODASAYISI
+        {
+            get { return odasayisi; }
+            set { odasayisi = Math.Abs(value); }
+        }
+
+        public int KATNUMARA
+        {
+            get { return katnumara; }
+            set { katnumara = Math.Abs(value); }
+        }
+
+        public double ALAN
+        {
+            get { return alan; }
+            set { alan = Math.Abs(value); }
+        }
+    }
+}
---
+++ C#/C#-Console/37-Siniflar_Kapsulleme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Siniflar_Kapsulleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Emlak eml = new Emlak();
+
+            eml.SEMT = "Pendik"; //Büyük harflerle gelecek
+            eml.RENK = "Mavi"; //Küçük harflerle gelecek
+            eml.ODASAYISI = -4; //Math.Abs() metodu sayesinde 4 olacak
+            eml.KATNUMARA = 8;
+            eml.ALAN = -90; //Math.Abs() metodu sayesinde 90 olacak
+
+            Console.WriteLine("Evin bulunduğu semt: " + eml.SEMT);
+            Console.WriteLine("Evin oda renkleri: " + eml.RENK);
+            Console.WriteLine("Evin toplam oda sayısı: " + eml.ODASAYISI);
+            Console.WriteLine("Evin bulunduğu kat: " + eml.KATNUMARA);
+            Console.WriteLine("Evin toplam alanı: " + eml.ALAN+" Metrekare");
+
+            Console.ReadLine();
+        }
+    }
+}
