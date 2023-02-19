d44d9ea354c0bf9a1ebbe37dbf0bfa9c8c5e9ddd
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-19 18:58:28 +0300

    Add files via upload

---
+++ C#/C#-Console/40-Ref_Out/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40_Ref_Out
+{
+    class Program
+    {
+
+        static void arttir(ref int s)
+        {
+            s++;
+        }
+
+        static void azalt(out int n)
+        {
+            n = 12;
+            n--;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int a = 9;
+            arttir(ref a); //ref kullanmazsak a=10 olmayacak, a=9 olarak kalacaktı.
+            Console.WriteLine(a);
+
+            int x; //Değişkenin değerini metodun içerisinde tanımlamıştık
+            azalt(out x);
+            Console.WriteLine(x);
+            Console.Read();
+        }
+    }
+}
