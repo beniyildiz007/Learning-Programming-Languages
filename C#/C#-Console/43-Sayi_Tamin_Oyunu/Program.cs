e9703d64955c695caafefea34b539ea02a0163ac
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-21 21:29:13 +0300

    Add files via upload

---
+++ C#/C#-Console/43-Sayi_Tamin_Oyunu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_Sayi_Tamin_Oyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tahmin = 0;
+            int hafiza;
+            int sayac = 0;
+            Random rastgele = new Random();
+            hafiza = rastgele.Next(1, 1000);
+            while (tahmin != hafiza)
+            {
+                sayac++;
+                Console.Write("Sayı Girin: ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin > hafiza)
+                {
+                    Console.WriteLine("Daha küçük sayı girin!");
+                }
+                else if (tahmin < hafiza)
+                {
+                    Console.WriteLine("Daha büyük sayı girin!");
+                }
+            }
+            Console.WriteLine("Tebriklerrr...!!!");
+            Console.WriteLine("{0}. denemede buldunuz!", sayac);
+            Console.ReadLine();
+        }
+    }
+}
