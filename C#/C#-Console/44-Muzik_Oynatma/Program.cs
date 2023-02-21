83fe134da2a23644c039b2e594d3dfa370631bf4
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-21 21:32:08 +0300

    Add files via upload

---
+++ C#/C#-Console/44-Muzik_Oynatma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media; //Müzik dosyasını oynatmak için öncelikle gerekli olan media kütüphanemizi ekliyoruz
+
+namespace _44_Muzik_Oynatma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Arkaplanda .wav uzantılı ses dosyalarının çalıştırılması için:
+            SoundPlayer ses = new SoundPlayer();
+            string yol = @"C:\Users\nihat\OneDrive\Belgeler\GitHub\Learning-Programming-Languages\C#\C#-Console\44-Muzik_Oynatma\Sainte Vie - Darko.wav";
+            ses.SoundLocation = yol;
+            ses.Play();
+            Console.Read();
+            */
+
+
+
+            //Windows Media Player açılarak ses dosyasının çalınması için:
+            System.Diagnostics.Process.Start("C:\\Users\\nihat\\OneDrive\\Belgeler\\GitHub\\Learning-Programming-Languages\\C#\\C#-Console\\44-Muzik_Oynatma\\Sainte Vie - Darko.mp3");
+            //Dosya yolunu belli etmek için ya başına @ işareti konulmalı ya da ters slash (\) işaretleri 2 kere yazılmalıdır.
+        }
+    }
+}
