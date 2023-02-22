9e48041587d89554f627221a6ea15ea17f8747d7
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-22 19:07:13 +0300

    Add files via upload

---
+++ C#/C#-Console/48-Metin_Belgesi_Kayit_Uygulamasi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //Input/Output kütüphanesini entegre ediyoruz
+
+namespace _48_Metin_Belgesi_Kayit_Uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dosya adı ne olsun?: ");
+            string dadi = Console.ReadLine();
+            StreamWriter dosya = new StreamWriter(@"C:\Users\nihat\OneDrive\Belgeler\GitHub\Learning-Programming-Languages\C#\C#-Console\48-Metin_Belgesi_Kayit_Uygulamasi\gunluk\" + dadi + ".txt");
+            Console.WriteLine("Buraya notunuzu yazın: ");
+            string veri = Console.ReadLine();
+            dosya.WriteLine(veri);
+            dosya.Close();
+        }
+    }
+}
