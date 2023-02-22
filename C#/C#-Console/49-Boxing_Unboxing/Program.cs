c70a7e5e88470c59ecaba75955e6216863534825
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-22 19:07:35 +0300

    Add files via upload

---
+++ C#/C#-Console/49-Boxing_Unboxing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49_Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing: Küçük veri türünü büyüğe koymak
+            int i = 5;
+            object o1 = i;
+            Console.WriteLine(o1);
+            //Output = 5
+
+
+
+            //Unboxing: Büyük veri türünü küçüğe sığdırmaya çalışmak
+            object o2 = 11;
+            //int k = o2; //Bu şekilde direkt olarak eşitleme yapılamaz
+            int k = Convert.ToInt32(o2);
+            Console.WriteLine(o2);
+            //Output = 11
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
