e1ec0f252c05c2c53db80be083f5cdc14bb7b127
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-24 13:17:29 +0300

    Add files via upload

---
+++ C/Kaynak-01/10-Donguler_1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	/*
+	// Ekrana 20 kere Merhaba Antalya yazdýran program
+	int k;
+	for(k=1;k<=20;k++){
+		printf("%d - Merhaba Antalya\n",k);
+	}
+	*/
+	
+	
+	
+	
+	int i;
+	for(i=0;i<=30;i+=5){ // i deðerimiz 5'er 5'er artsýn
+		printf("%d - iyi calismalar dilerim\n",i); // 7 defa yazar çünkü 0 ve 30 dahildir
+	}
+	
+	
+	
+	
+	return 0;
+}
