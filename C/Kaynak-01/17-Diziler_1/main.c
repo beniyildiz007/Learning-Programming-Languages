83f02e408f34b84970bddc6304bd6217d1ed4b53
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-26 02:52:41 +0300

    Add files via upload

---
+++ C/Kaynak-01/17-Diziler_1/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	/*
+	int tekrakamlar[]={1,3,5,7,9};
+	//printf("%d",tekrakamlar); // Output = 6487552
+	printf("%d",tekrakamlar[0]); // Output = 1
+	*/
+	
+	
+	
+	/*
+	char sehir[]={'a','n','k','a','r','a'};
+	printf("%s",sehir); // Output = ankara
+	*/
+	
+	
+	
+	
+	/*
+	char sehir[4]={'a','n','k','a','r','a'};
+	printf("%s",sehir); // Output = anka
+	*/
+	
+	
+	
+	
+	int sayilar[4]; // Dizimizin 4 elemanlý bir dizi olacaðýný söylüyoruz
+	sayilar[0]=24;
+	sayilar[1]=895;
+	sayilar[2]=774;
+	sayilar[3]=662;
+	printf("%d",sayilar[2]); // Output = 774	
+	
+	
+	
+	
+	return 0;
+}
