728754ce7ab258be0daea9aa1748add3662cc3cb
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-26 02:53:29 +0300

    Add files via upload

---
+++ C/Kaynak-01/20-Yildizlarla_Sekil_Olusturma_1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	//Yýldýzlarla dik üçgen oluþturma
+	int i,j;
+	for(i=1;i<=10;i++){
+		
+		for(j=1;j<=i;j++){
+			
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	
+	return 0;
+}
