869a018b5734c83c6be2eb9f8370e0ed00ae5447
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-02-27 06:15:27 +0300

    Add files via upload

---
+++ C/Kaynak-01/23-Cok_Boyutlu_Diziler_1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	
+	int dizi[2][2];
+	
+	dizi[0][0]=10;
+	dizi[0][1]=20;
+	dizi[1][0]=30;
+	dizi[1][1]=40;
+	
+	printf("Matrisi 0-0 degeri: %d\n",dizi[0][0]);
+	printf("Matrisi 0-1 degeri: %d\n",dizi[0][1]);
+	printf("Matrisi 1-0 degeri: %d\n",dizi[1][0]);
+	printf("Matrisi 1-1 degeri: %d\n",dizi[1][1]);
+	
+	
+	
+	return 0;
+}
