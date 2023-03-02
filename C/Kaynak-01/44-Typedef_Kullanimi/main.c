8445241e323eb323d70a8e6465dbb3a20f3c036d
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-03 01:38:53 +0300

    Add files via upload

---
+++ C/Kaynak-01/44-Typedef_Kullanimi/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+typedef int tamsayi;
+
+typedef int deger;
+
+int main() {
+	
+	
+	//C# dilindeki class çaðýrýp kullanmaya benziyor
+	tamsayi ts,ts2,ts3;
+	ts=22;
+	ts2=23;
+	ts3=26;
+	printf("%d %d %d",ts,ts2,ts3);	
+	
+	
+	
+	deger d1,d2;
+	int toplam;
+	
+	d1=20;
+	d2=40;
+	
+	toplam=d1+d2;
+	
+	printf("\n\nToplam Deger: %d",toplam);
+	
+	
+	
+	
+	return 0;
+}
