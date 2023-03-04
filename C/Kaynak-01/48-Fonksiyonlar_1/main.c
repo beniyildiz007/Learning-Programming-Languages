0551916d5125cde0c449bd07638bbabc17ff68d6
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-04 16:36:09 +0300

    Add files via upload

---
+++ C/Kaynak-01/48-Fonksiyonlar_1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//Geriye deðer döndürmeyen fonksiyonlar için void kullanýlýr
+//Void boþ demektir
+void listele(){
+	printf("Berkan Nihat Yýldýz\n");
+	printf("Ecmel Yýldýz\n");
+	printf("Ecrin Yýldýz\n");
+	printf("Mert Yýldýz\n");
+	printf("Tunahan Yýldýz\n");
+}
+
+
+
+int main() {
+	
+	
+	//Þimdi fonksiyonumuzu çaðýrýyoruz
+	listele();
+	
+	
+	return 0;
+}
