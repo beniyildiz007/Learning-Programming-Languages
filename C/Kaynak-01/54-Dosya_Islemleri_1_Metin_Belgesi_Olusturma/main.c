18cfaa2a714dbdabe688c9fa653b6042be586fb8
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-05 01:24:43 +0300

    Add files via upload

---
+++ C/Kaynak-01/54-Dosya_Islemleri_1_Metin_Belgesi_Olusturma/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	FILE *dosya;
+	dosya=fopen("C:\\Users\\nihat\\OneDrive\\Belgeler\\GitHub\\Learning-Programming-Languages\\C\\54-Dosya_Islemleri_1_Metin_Belgesi_Olusturma\\deneme.txt","w");
+	//deneme.txt isimli boþ bir dosya oluþturduk	
+	
+	
+	return 0;
+}
