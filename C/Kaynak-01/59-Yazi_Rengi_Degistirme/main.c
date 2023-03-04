3c9caa272700eb4e4c42ca95d78ab57ecdea67dc
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-05 01:26:54 +0300

    Add files via upload

---
+++ C/Kaynak-01/59-Yazi_Rengi_Degistirme/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	
+	printf("On binlerce insanimizin vefat ettigi 6 subat depreminin hesabi sorulacak.");
+	system("COLOR 0");
+	system("COLOR 1");
+	system("COLOR 2");
+	system("COLOR 3");
+	system("COLOR 4");
+	system("COLOR 5");
+	system("COLOR 6");
+	system("COLOR 7");
+	system("COLOR 8");
+	system("COLOR 9");
+	system("COLOR A");
+	system("COLOR B");
+	system("COLOR C");
+	system("COLOR D");
+	system("COLOR E");
+	system("COLOR F");
+	
+	
+	return 0;
+}
