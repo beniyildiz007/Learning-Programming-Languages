453a32efbb65ff7949e4b5e93c5ba6f569933ce1
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-23 17:19:47 +0300

    Add files via upload

---
+++ C#/C#-Forms/16-N_Katmanli_Mimari_PROJE/16-N_Katmanli_Mimari_PROJE/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityLayer;
+using DataAccessLayer;
+using LogicLayer;
+
+namespace _16_N_Katmanli_Mimari_PROJE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            List<EntityPersonel> PerList = LogicPersonel.LLPersonelListesi();
+            dataGridView1.DataSource = PerList;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Ad = txtAd.Text;
+            ent.Soyad = txtSoyad.Text;
+            ent.Sehir = txtSehir.Text;
+            ent.Gorev = txtGorev.Text;
+            ent.Maas = short.Parse(txtMaas.Text);
+
+            LogicPersonel.LLPersonelEkle(ent);
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(txtID.Text);
+            LogicPersonel.LLPersonelSil(ent.Id);
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+
+            ent.Id = Convert.ToInt32(txtID.Text);
+            ent.Ad = txtAd.Text;
+            ent.Soyad = txtSoyad.Text;
+            ent.Sehir = txtSehir.Text;
+            ent.Gorev = txtGorev.Text;
+            ent.Maas = short.Parse(txtMaas.Text);
+
+            LogicPersonel.LLPersonelGuncelle(ent);
+        }
+    }
+}
---
+++ C#/C#-Forms/16-N_Katmanli_Mimari_PROJE/LogicLayer/LogicPersonel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataAccessLayer;
+
+namespace LogicLayer
+{
+    public class LogicPersonel
+    {
+        public static List<EntityPersonel> LLPersonelListesi()
+        {
+            return DALPersonel.PersonelListesi();
+        }
+
+        public static int LLPersonelEkle(EntityPersonel p)
+        {
+            if(p.Ad!="" && p.Soyad!="" && p.Maas>=8500 && p.Ad.Length >= 3)
+            {
+                return DALPersonel.PersonelEkle(p);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static bool LLPersonelSil(int per)
+        {
+            if (per >= 1)
+            {
+                return DALPersonel.PersonelSil(per);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool LLPersonelGuncelle(EntityPersonel ent)
+        {
+            if (ent.Ad.Length>=3 && ent.Ad!="" && ent.Maas >= 8500)
+            {
+                return DALPersonel.PersonelGuncelle(ent);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
