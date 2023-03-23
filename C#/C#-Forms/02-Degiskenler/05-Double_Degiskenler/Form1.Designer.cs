b329aea8f13175201bb3b4343744e29fdf1daaec
Berkan Nihat YILDIZ <95151751+beniyildiz007@users.noreply.github.com>
2023-03-23 16:40:42 +0300

    Add files via upload

---
+++ C#/C#-Forms/02-Degiskenler/05-Double_Degiskenler/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace _05_Double_Degiskenler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ortalama Sonucu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 491);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ C#/C#-Forms/02-Degiskenler/05-Double_Degiskenler/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_Double_Degiskenler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s1, s2, s3, ort;
+            s1 = 60;
+            s2 = 50;
+            s3 = 78;
+            ort = (s1 + s2 + s3) / 3;
+            label1.Text = ort.ToString("0.00"); //.ToString içerisine yazdığımız sıfırlar, sonucun nasıl bir formatta yazılması gerektiğini belirler
+        }
+    }
+}
